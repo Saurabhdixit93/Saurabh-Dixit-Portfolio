637c500894db0576d35b109f59ba2acded7b7f8e
Saurabh Dixit <86460997+Saurabhdixit93@users.noreply.github.com>
2024-09-29 19:31:04 +0530

    update: Production Ready Code

---
+++ data/index.ts
@@ -0,0 +1,304 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "React Chat App - Realtime Chat Application",
+    pinTitle: "React Chat",
+    des: " A realtime chat application that allows users to communicate with each other. It's built with React, and Firebase.",
+    img: "/project1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/firbase.svg"],
+    link: "https://reactchatawebpp.netlify.app",
+  },
+  {
+    id: 2,
+    title: "Google Calendar System",
+    pinTitle: "Calendar App",
+    des: "Developed a custom calendar application to streamline scheduling and event management, integrating Google Calendar APIs for seamless syncing. Utilized personal expertise and online resources to ensure a smooth user experience and reliable functionality.",
+    img: "/project2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/calendar.svg"],
+    link: "https://user-calander-system.netlify.app",
+  },
+  {
+    id: 3,
+    title: "Banking Landing Page",
+    pinTitle: "Landing Page",
+    des: "Designed and developed a responsive single-page application for a banking service, utilizing Next.js and Tailwind CSS. The app features AI capabilities, an integrated payments and credits system, and a custom font to enhance branding and user experience, showcasing a modern tech stack.",
+    img: "/project3.svg",
+    iconLists: ["/next.svg", "/tail.svg"],
+    link: "https://banking-app-landing-page.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "FarmOs Project",
+    pinTitle: "FarmOs",
+    des: "Implemented FarmOS, an open-source farm management system that significantly enhanced data collection processes. This implementation reduced manual data entry by 40% and provided actionable insights, facilitating better decision-making for farm management.",
+    img: "/farmos.svg",
+    iconLists: ["/next.svg", "/tail.svg", "node.svg"],
+    link: "https://farmos.sensegrass.com",
+  },
+  {
+    id: 5,
+    title: "CRUD Management API",
+    pinTitle: "CRUD API",
+    des: "Developed a fully authenticated CRUD Management API using Node.js and Sequelize for efficient database interactions. Implemented robust authentication mechanisms and utilized sequelize-cli for database migrations, ensuring smooth data management and integrity.",
+    img: "/ApiImage.svg",
+    iconLists: ["/node.svg"],
+    link: "https://authenticate-assignment-api.onrender.com",
+  },
+  {
+    id: 6,
+    title: "YouTube Audio and Video Download",
+    pinTitle: "YouTube API",
+    des: "Created a web application for downloading audio and video from YouTube using Node.js and Express, employing EJS for dynamic templating. The application offers a user-friendly interface without relying on React.js, ensuring a seamless experience for users seeking to download multimedia content.",
+    img: "/Project5.svg",
+    iconLists: ["/node.svg", "/expre.svg"],
+    link: "https://youtube-converter-losg.onrender.com/",
+  },
+  {
+    id: 7,
+    title: "My Portfolio",
+    pinTitle: "Portfolio",
+    des: "Built a portfolio website using Next.js and Tailwind CSS, ensuring a visually captivating user experience.",
+    img: "/Project6.svg",
+    iconLists: ["/next.svg", "/tail.svg"],
+    link: "https://saurabh-portfolio.vercel.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Working with Saurabh was an incredible experience. His attention to detail and deep understanding of technology helped our project reach new heights. He constantly delivered on time and exceeded our expectations.",
+    name: "Emily Carter",
+    title: "CEO of TechWave Solutions",
+  },
+  {
+    quote:
+      "Saurabh's expertise in web development transformed our platform into a high-performing, user-friendly site. His ability to solve complex problems quickly made a significant impact on our product's success.",
+    name: "John Mitchell",
+    title: "CTO at Innovate Labs",
+  },
+  {
+    quote:
+      "Saurabh brings a blend of technical skill and creativity that is hard to find. His insights and innovative approach made our project a success. I highly recommend him for any development work.",
+    name: "Sarah Lewis",
+    title: "Product Manager at Digital Horizons",
+  },
+  {
+    quote:
+      "Saurabh’s dedication to his craft is remarkable. He consistently goes above and beyond to ensure the project meets both technical and business goals. A true professional in every sense.",
+    name: "David Cooper",
+    title: "Lead Engineer at BrightCode Systems",
+  },
+  {
+    quote:
+      "We couldn't be happier with the work Saurabh delivered. His focus on performance and scalability helped us build a robust platform that has improved our customer engagement tremendously.",
+    name: "Laura Smith",
+    title: "VP of Engineering at CloudGenix",
+  },
+  {
+    quote:
+      "Saurabh’s approach to problem-solving is top-notch. He helped us navigate a number of technical challenges with ease, and his proactive communication made the entire process seamless.",
+    name: "Michael Ross",
+    title: "Founder of Velocity Networks",
+  },
+  {
+    quote:
+      "From the start, Saurabh demonstrated a level of professionalism and technical know-how that put us at ease. His work was instrumental in helping us scale our platform for larger audiences.",
+    name: "Olivia Davis",
+    title: "COO at NextGen Ventures",
+  },
+  {
+    quote:
+      "Saurabh delivered an exceptional solution for our business. His ability to translate our needs into a fully functional application was impressive, and he exceeded our expectations at every step.",
+    name: "Christopher Brown",
+    title: "Head of Technology at AlphaWave Digital",
+  },
+  {
+    quote:
+      "Saurabh has a talent for taking complex requirements and simplifying them into efficient, elegant solutions. His commitment to quality and performance was evident throughout the project.",
+    name: "Emma Wilson",
+    title: "Project Lead at CodeCore Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Saurabh was a game-changer for our development team. His technical prowess and ability to communicate complex ideas made the entire process smooth and successful.",
+    name: "James Martin",
+    title: "CTO at FusionPoint Software",
+  },
+  {
+    quote:
+      "Saurabh’s professionalism and enthusiasm for every part of the development process was inspiring. He not only delivered high-quality work but also provided valuable insights that improved our final product.",
+    name: "Sophia Evans",
+    title: "CEO at QuantumLeap Solutions",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Teaching Assistant - JAVA",
+    desc: "Assisted in teaching core Java concepts, conducted coding workshops, and provided support to students by reviewing assignments and troubleshooting code-related issues.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+    company: "Coding Ninjas",
+  },
+  {
+    id: 2,
+    title: "Junior NodeJS Developer Intern",
+    desc: "Contributed to the backend development using Node.js, improving system performance and ensuring seamless integration with front-end services.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+    company: "Kenmark ITan Solutions",
+  },
+  {
+    id: 3,
+    title: "Freelance Full Stack Project",
+    desc: "Led the full-stack development of a web application for a client, managing both front-end and back-end components, from initial concept to deployment, ensuring a smooth user experience and scalable architecture.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+    company: "Freelance",
+  },
+  {
+    id: 4,
+    title: "Full Stack Engineer",
+    desc: "Designed, developed, and maintained both front-end and back-end features using modern technologies. Collaborated with cross-functional teams to build scalable web applications, ensuring high performance and a seamless user experience.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+    company: "Sensegrass",
+  },
+  {
+    id: 5,
+    title: "Senior Software Development Engineer",
+    desc: "Led the design and development of complex web applications using React.js, Redux, and TypeScript. Ensured optimal performance, seamless user experience, and robust integration with backend services while mentoring junior developers and driving architectural decisions.",
+    className: "md:col-span-2",
+    thumbnail: "/exp5.svg",
+    company: "Sensegrass",
+  },
+];
+const phoneNumber = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER;
+const message = "Hello! I would like to chat with you.";
+
+const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+  message
+)}`;
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Saurabhdixit93",
+  },
+  {
+    id: 2,
+    img: "/wha.svg",
+    link: `${whatsappUrl}`,
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/saurabhdixit93/",
+  },
+];
