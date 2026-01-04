1e3898b4f7c5f47ac74b3e016388da3ad013d13b
saurabh-sensegrass <saurabh@sensegrass.com>
2026-01-04 17:12:05 +0530

    feat: configure security headers and disable Next.js powered by header and production browser source maps.

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,42 @@ const nextConfig = {
     ignoreBuildErrors: false,
   },
   output: "standalone",
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self' data:; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; upgrade-insecure-requests;",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "camera=(), microphone=(), geolocation=()",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
